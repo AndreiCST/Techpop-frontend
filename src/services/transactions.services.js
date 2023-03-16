c7d9f03def60e43c498999ad93c20ec9a865a72f
Miguel Ferragut <mferracab@gmail.com>
2023-03-16 12:22:58 +0100

    transactions

---
+++ src/services/transactions.services.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+class TransactionsService {
+
+    constructor() {
+        this.api = axios.create({
+            baseURL: `${process.env.REACT_APP_API_URL}/transactions`
+        })
+    }
+
+    startTrans(product_id, buyer_id, seller_id) {
+        return this.api.post(`/start/${product_id}/${buyer_id}/${seller_id}`)
+    }
+
+    rejectTrans(transaction_id) {
+        return this.api.put(`/reject/${transaction_id}`)
+    }
+
+    acceptTrans(transaction_id, product_id, buyer_id, seller_id) {
+        return this.api.put(`/accept/${transaction_id}/${product_id}/${buyer_id}/${seller_id}`)
+    }
+
+}
+
+const transactionService = new TransactionsService()
+
+export default transactionService
