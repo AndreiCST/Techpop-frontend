9f1702d430052c1ef58cfba9a99beafeb9b18fb4
Miguel Ferragut <mferracab@gmail.com>
2023-03-17 10:10:59 +0100

    carousel changes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -15,13 +15,16 @@ const HomePage = () => {
     }, [])
 
 
+
+
     const loadProducts = () => {
 
         productService
             .getProducts()
             .then(({ data }) => {
                 setProducts(data)
-                const imagesArray = data.map(product => product.images)
+                const imagesArray = data?.map(product => product.images)
+                console.log(imagesArray)
                 setProductsImg(imagesArray)
             })
             .catch(err => console.log(err))
@@ -31,15 +34,15 @@ const HomePage = () => {
         <>
             <h1 className='mt-5 pagePos'>Let´s buy Tech</h1>
 
-            <Container md={{ span: 8, offset: 2 }}>
-                <Carousel interval={5000}>
+            <Container md={{ span: 6, offset: 3 }} style={{ margin: 'auto', textAlign: 'center', display: 'flex', justifyContent: 'center' }} className="mb-3">
+                <Carousel interval={5000} wrap={true}>
                     {
                         productsImg?.map((elm, index) => {
                             return (
-                                <Carousel.Item key={index} style={{ width: '100%', height: '600px', backgroundColor: '#f5f5f5' }}>
+                                <Carousel.Item key={index} style={{ width: '100%', height: '650px', backgroundColor: '#f5f5f5' }}>
                                     <img
                                         className="d-block w-100"
-                                        src={`${elm}`}
+                                        src={elm[0]}
                                         alt={`Slide ${index}`}
                                         style={{ objectFit: 'fit', width: '100%', height: '100%' }}
                                     />
